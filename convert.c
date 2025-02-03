7a066a0de169e7e8469c59adc8e1aef26663bdbd
NeonCrafter13 <neon.crafter.xd@gmail.com>
2025-02-03 21:37:52 +0100

    Implmented using linked lists

--- convert.c
+++ convert.c
@@ -12,7 +12,7 @@ int main()
 {
     char ch, file_name[25];
     FILE *fp;
-    
+
 
     printf("Enter name of a file you wish to compile to C\n");
     gets(file_name);
@@ -30,36 +30,64 @@ int main()
 
     print_to_out("#include <stdio.h>");
     print_to_out("#include <stdlib.h>");
+
+    print_to_out(
+    "typedef struct DoubleLinkedList{"
+        "struct DoubleLinkedList *prev;"
+        "unsigned int value;"
+        "struct DoubleLinkedList *next;"
+    "} DoubleLinkedList;"
+    );
+
     print_to_out("int main(){");
-    print_to_out("u_int8_t memory[255] = {};");
-    print_to_out("u_int8_t pointer = 0;");
+    print_to_out("DoubleLinkedList* current = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));");
+    print_to_out("current->value = 0;");
+    print_to_out("current->prev = NULL;");
+    print_to_out("current->next = NULL;");
+
     while ((ch = fgetc(fp)) != EOF)
     {
         printf("%c", ch);
         switch(ch) {
             case '[':
-                print_to_out("while (memory[pointer] != 0) {");
+                print_to_out("while (current->value != 0) {");
                 break;
             case ']':
                 print_to_out("}");
                 break;
             case '+':
-                print_to_out("memory[pointer]++;");
+                print_to_out("current->value++;");
                 break;
             case '-':
-                print_to_out("memory[pointer]--;");
+                print_to_out("current->value--;");
                 break;
             case '<':
-                print_to_out("pointer--;");
+                print_to_out(
+                "if(current->prev == NULL){"
+                    "DoubleLinkedList *a = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));"
+                    "a->prev = NULL;"
+                    "a->next = current;"
+                    "a->value = 0;"
+                    "current->prev = a;"
+                "}"
+                "current = current->prev;");
                 break;
             case '>':
-                print_to_out("pointer++;");
+                print_to_out(
+                "if(current->next == NULL){"
+                    "DoubleLinkedList *a = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));"
+                    "a->next = NULL;"
+                    "a->prev = current;"
+                    "a->value = 0;"
+                    "current->next = a;"
+                "}"
+                "current = current->next;");
                 break;
             case '.':
-                print_to_out("printf(\"%c\", memory[pointer]);");
+                print_to_out("printf(\"%c\", current->value);");
                 break;
             case ',':
-                print_to_out("memory[pointer] = getchar();");
+                print_to_out("current->value = getchar();");
                 break;
             default:
                 break;
